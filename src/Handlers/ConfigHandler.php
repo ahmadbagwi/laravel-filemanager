c1097ce37b47d447042a0db63254d9629b3c452d
Ahmad Bagwi Rifai <ahmad.kdesain@gmail.com>
2020-09-26 12:48:46 +0700

    Multiuser, superadmin can access user files

--- src/Handlers/ConfigHandler.php
+++ src/Handlers/ConfigHandler.php
@@ -1,11 +1,23 @@
 <?php
 
 namespace UniSharp\LaravelFilemanager\Handlers;
+use Illuminate\Support\Str;
 
 class ConfigHandler
 {
     public function userField()
     {
-        return auth()->id();
+        // I don't know LfmConfigHandler.php not working after modified following documentation, so I edit here
+        // superadmin can access to user directory, make sure chmod 777 -R files
+        // user directory using name with slug, easy superadmin to browse files
+        $name = auth()->user()->name;
+        $name_slug = Str::slug($name, '_');
+        $role = auth()->user()->role;
+
+        if($role == "superadmin"){
+          return 'superadmin';
+        }else{
+          return 'superadmin/'.$name_slug;
+        }
     }
 }
